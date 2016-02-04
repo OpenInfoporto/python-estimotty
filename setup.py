e63bb668b4291c8665f119bba6225d2cd897aa78
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2016-02-04 18:24:26 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='estimotty',
-      version='0.1',
+      version='0.1.3',
       description='Estimote cloud client',
       url='https://github.com/OpenInfoporto/python-estimotty.git',
       author='Andrea Carmisciano',
       author_email='andrea.carmisciano@infoporto.it',
       license='MIT',
       packages=['estimotty'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=['requests'])
